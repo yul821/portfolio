fb850eca8151d89aaddd54a4fc5ef2f811bfae7b
yul821 <wxl750@naver.com>
2025-03-25 12:58:32 +0900

    pm 12:58

--- work/page/js/sub.js
+++
@@ -1,9 +0,0 @@
-// top 버튼 
-$(document).ready(function() {
-    $('.top-btn a').click(function(event) {
-        event.preventDefault(); // 기본 클릭 동작 방지
-        $('html, body').animate({
-            scrollTop: $('#header-top').offset().top // 헤더로 스크롤
-        }, 1000); // 1000ms 동안 부드럽게 스크롤
-    });
-});
